c7355b147eb85e1261c45a56d917b219aaa043d2
seanheller24 <hellerse@bc.edu>
2022-01-31 18:14:55 -0500

    Initial Commit

---
+++ Week Two/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week Two
+//
+//  Created by Sean Heller on 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
